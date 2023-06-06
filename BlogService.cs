9d4e27296a0723c630f618532d384489a9f0bd05
alyoneek <alyonta03@mail.ru>
2023-06-06 21:41:53 +0700

    Change gitignore and add missed BlogService file

---
+++ BlogService.cs
@@ -0,0 +1,38 @@
+﻿using Blog.Dto;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blog
+{
+    public static class BlogService
+    {
+        public static ICollection<NumberOfCommentsDto> NumberOfCommentsPerUser(MyDbContext context)
+        {
+            return context.BlogComments
+               .GroupBy(c => c.UserName)
+               .Select(c => new NumberOfCommentsDto { UserName = c.Key, NumberOfComments = c.Count() })
+               .OrderBy(c => c.NumberOfComments)
+               .ToList();
+        }
+
+        public static ICollection<PostDto> PostsOrderedByLastCommentDate(MyDbContext context)
+        {
+            return context.BlogPosts
+               .OrderByDescending(p => p.Comments.OrderByDescending(c => c.CreatedDate).First().CreatedDate)
+               .Select(p => new PostDto(p))
+               .ToList();
+        }
+
+        public static ICollection<NumberOfCommentsDto> NumberOfLastCommentsLeftByUser(MyDbContext context)
+        {
+            return context.BlogComments
+               .Where(c => c.CreatedDate == c.BlogPost.Comments.Max(c => c.CreatedDate))
+               .GroupBy(c => c.UserName)
+               .Select(c => new NumberOfCommentsDto { UserName = c.Key, NumberOfComments = c.Count() })
+               .OrderBy(c => c.NumberOfComments)
+               .ToList();
+        }
+    }
+}
